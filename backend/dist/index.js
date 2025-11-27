d7477bd14bd0539fe396ddbba570d7c4ea5e5c47
Aditya Gupta <realadityagupta2@gmail.com>
2025-11-27 10:56:13 +0530

    database connection established

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./utilis/db.js";
+dotenv.config();
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.listen(port, () => {
+    connectDB();
     console.log("Starting server...");
     console.log(`Server is running at http://localhost:${port}`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./utilis/db.js";
+dotenv.config();
 const app = express();
 const port = 3000;
 
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 app.listen(port, () => {
-    console.log("Starting server...");
+  connectDB();
+  console.log("Starting server...");
   console.log(`Server is running at http://localhost:${port}`);
 });
-
