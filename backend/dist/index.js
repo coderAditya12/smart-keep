55d334534602bdd5c37be66b1fe1d93d6d15c5f7
Aditya Gupta <realadityagupta2@gmail.com>
2025-11-28 15:16:24 +0530

    env file updated

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,7 +6,7 @@ import aiRoute from "./route/ArticleRoute.js";
 import userRoute from "./route/userRoute.js";
 import cors from "cors";
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(cors({
     origin: "*",
     methods: "*",
